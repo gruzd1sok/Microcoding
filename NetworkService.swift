8524c08db338e9ef6fffacbe56581fb5a5c6bacf
Ilya Gruzdev <ily.gruzdev99@gmail.com>
2021-06-12 01:07:17 +0700

    Create NetworkService.swift
    Service to using RESTful API in future

---
+++ NetworkService.swift
@@ -0,0 +1,62 @@
+//
+//  Network Service.swift
+//  AuthTest
+//
+//  Created by Илья Груздев on 11.06.2021.
+//
+
+import Foundation
+
+enum AuthenticationError: Error {
+    case invalidCredentials
+    case custom(errorMessage: String)
+}
+
+struct LoginRequestBody: Codable {
+    let email: String
+    let password: String
+}
+
+struct LoginResponse: Codable {
+    let token: String?
+    let message: String?
+    let success: Bool?
+}
+
+class NetworkService {
+    func login(email: String, password: String, completion: @escaping (Result<String, AuthenticationError>) -> Void) {
+        
+        guard let url = URL(string: "{{URL}}") else {
+            completion(.failure(.custom(errorMessage: "URL is not correct")))
+            return
+        }
+        
+        let body = LoginRequestBody(email: email, password: password)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("ru", forHTTPHeaderField: "Accept-Language")
+        request.httpBody = try? JSONEncoder().encode(body)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil else {
+                completion(.failure(.custom(errorMessage: "No data")))
+                return
+            }
+            
+            guard let loginResponse = try? JSONDecoder().decode(LoginResponse.self, from: data) else {
+                completion(.failure(.invalidCredentials))
+                return
+            }
+            
+            guard let token = loginResponse.token else {
+                completion(.failure(.invalidCredentials))
+                return
+            }
+            
+            completion(.success(token))
+            
+        }.resume()
+    }
+}
